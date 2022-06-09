84fcd10f0fc9ad8d9ce3b5fee79d7ce54dab0319
shazrapwc <105486103+shazrapwc@users.noreply.github.com>
2022-06-09 12:25:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 * @param {Object} req Cloud Function request context.
 * @param {Object} res Cloud Function response context.
 */
-exports.helloGET = (req, res) => {
-    
+exports.helloGET = (req, res) => {  
     res.send('Hello from Cloud Functions and Cloud Source Repositories');
       
   };
